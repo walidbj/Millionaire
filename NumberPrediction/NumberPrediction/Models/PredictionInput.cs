2ec83db29f1089a6c5f004bc2722e410d7865cf7
Walid Ben Jemia <walidelli@hotmail.com>
2019-12-11 16:37:45 -0500

    Various fixes

---
+++ NumberPrediction/NumberPrediction/Models/PredictionInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberPrediction.Models
+{
+    public class PredictionInput
+    {
+        public DateTime InputDate { get; set; }
+        public bool IncludeDay { get; set; }
+        public bool IncludeMonth { get; set; }
+        public bool IncludeWeek { get; set; }
+    }
+}
